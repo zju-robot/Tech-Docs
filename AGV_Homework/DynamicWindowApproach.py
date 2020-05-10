3ca15fbae70f778019f158672d673b57114b8ced
Robotic-ZJU <44562831+Robotic-ZJU@users.noreply.github.com>
2020-05-10 21:43:11 +0800

    Create DynamicWindowApproach.py

---
+++ AGV_Homework/DynamicWindowApproach.py
@@ -0,0 +1,240 @@
+"""
+
+Mobile robot motion planning sample with Dynamic Window Approach
+
+For learning purposes only.
+
+Author:lyl
+
+"""
+
+import math
+from enum import Enum
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+show_animation = True
+
+
+def dwa_control(x, config, goal, ob):
+    """
+    Dynamic Window Approach control
+    """
+
+    dw = calc_dynamic_window(x, config)
+
+    u, trajectory = calc_control_and_trajectory(x, dw, config, goal, ob)
+
+    return u, trajectory
+
+
+class RobotType(Enum):
+    circle = 0
+    rectangle = 1
+
+
+class Config:
+    """
+    simulation parameter class
+    """
+
+    def __init__(self):
+        # robot parameter
+        self.max_speed = 1.0  # [m/s]
+        self.min_speed = -0.5  # [m/s]
+        self.max_yawrate = 40.0 * math.pi / 180.0  # [rad/s]
+        self.max_accel = 0.2  # [m/ss]
+        self.max_dyawrate = 40.0 * math.pi / 180.0  # [rad/ss]
+        self.v_reso = 0.01  # [m/s]
+        self.yawrate_reso = 0.1 * math.pi / 180.0  # [rad/s]
+        self.dt = 0.1  # [s] Time tick for motion prediction
+        self.predict_time = 3.0  # [s]
+        self.to_goal_cost_gain = 0.15
+        self.speed_cost_gain = 1.0
+        self.obstacle_cost_gain = 1.0
+        self.robot_type = RobotType.circle
+
+        # if robot_type == RobotType.circle
+        # Also used to check if goal is reached in both types
+        self.robot_radius = 1.0  # [m] for collision check
+
+        # if robot_type == RobotType.rectangle
+        self.robot_width = 0.5  # [m] for collision check
+        self.robot_length = 1.2  # [m] for collision check
+
+    @property
+    def robot_type(self):
+        return self._robot_type
+
+    @robot_type.setter
+    def robot_type(self, value):
+        if not isinstance(value, RobotType):
+            raise TypeError("robot_type must be an instance of RobotType")
+        self._robot_type = value
+
+
+def motion(x, u, dt):
+    """
+    motion model
+    """
+
+    # TODO
+
+    return x
+
+
+def calc_dynamic_window(x, config):
+    """
+    calculation dynamic window based on current state x
+    """
+
+    # Dynamic window from robot specification
+    # TODO
+
+    # Dynamic window from motion model
+    # TODO
+
+    #  [vmin, vmax, yaw_rate min, yaw_rate max]
+    # TODO
+
+    return dw
+
+
+def predict_trajectory(x_init, v, y, config):
+    """
+    predict trajectory with an input
+    """
+
+    # TODO
+
+    return traj
+
+
+def calc_control_and_trajectory(x, dw, config, goal, ob):
+    """
+    calculation final input with dynamic window
+    """
+
+    x_init = x[:]
+    min_cost = float("inf")
+    best_u = [0.0, 0.0]
+    best_trajectory = np.array([x])
+
+    # evaluate all trajectory with sampled input in dynamic window
+    # TODO
+    return best_u, best_trajectory
+
+
+def calc_obstacle_cost(trajectory, ob, config):
+    """
+        calc obstacle cost inf: collision
+    """
+    # TODO
+
+    return cost
+
+
+def calc_to_goal_cost(trajectory, goal):
+    """
+        calc to goal cost with angle difference
+    """
+
+    # TODO
+
+    return cost
+
+
+def plot_arrow(x, y, yaw, length=0.5, width=0.1):  # pragma: no cover
+    plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+              head_length=width, head_width=width)
+    plt.plot(x, y)
+
+
+def plot_robot(x, y, yaw, config):  # pragma: no cover
+    if config.robot_type == RobotType.rectangle:
+        outline = np.array([[-config.robot_length / 2, config.robot_length / 2,
+                             (config.robot_length / 2), -config.robot_length / 2,
+                             -config.robot_length / 2],
+                            [config.robot_width / 2, config.robot_width / 2,
+                             - config.robot_width / 2, -config.robot_width / 2,
+                             config.robot_width / 2]])
+        Rot1 = np.array([[math.cos(yaw), math.sin(yaw)],
+                         [-math.sin(yaw), math.cos(yaw)]])
+        outline = (outline.T.dot(Rot1)).T
+        outline[0, :] += x
+        outline[1, :] += y
+        plt.plot(np.array(outline[0, :]).flatten(),
+                 np.array(outline[1, :]).flatten(), "-k")
+    elif config.robot_type == RobotType.circle:
+        circle = plt.Circle((x, y), config.robot_radius, color="b")
+        plt.gcf().gca().add_artist(circle)
+        out_x, out_y = (np.array([x, y]) +
+                        np.array([np.cos(yaw), np.sin(yaw)]) * config.robot_radius)
+        plt.plot([x, out_x], [y, out_y], "-k")
+
+
+def main(gx=10.0, gy=10.0, robot_type=RobotType.circle):
+    print(__file__ + " start!!")
+    # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
+    x = np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])
+    # goal position [x(m), y(m)]
+    goal = np.array([gx, gy])
+    # obstacles [x(m) y(m), ....]
+    ob = np.array([[-1, -1],
+                   [0, 2],
+                   [4.0, 2.0],
+                   [5.0, 4.0],
+                   [5.0, 5.0],
+                   [5.0, 6.0],
+                   [8.0, 9.0],
+                   [7.0, 9.0],
+                   [7.0, 10.0],
+                   [9.0, 11.0],
+                   [12.0, 13.0],
+                   [13.0, 12.0],
+                   [15.0, 15.0],
+                   [13.0, 13.0]
+                   ])
+
+    # input [forward speed, yaw_rate]
+    config = Config()
+    config.robot_type = robot_type
+    trajectory = np.array(x)
+
+    while True:
+        u, predicted_trajectory = dwa_control(x, config, goal, ob)
+        x = motion(x, u, config.dt)  # simulate robot
+        trajectory = np.vstack((trajectory, x))  # store state history
+
+        if show_animation:
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect('key_release_event',
+                    lambda event: [exit(0) if event.key == 'escape' else None])
+            plt.plot(predicted_trajectory[:, 0], predicted_trajectory[:, 1], "-g")
+            plt.plot(x[0], x[1], "xr")
+            plt.plot(goal[0], goal[1], "xb")
+            plt.plot(ob[:, 0], ob[:, 1], "ok")
+            plot_robot(x[0], x[1], x[2], config)
+            plot_arrow(x[0], x[1], x[2])
+            plt.axis("equal")
+            plt.grid(True)
+            plt.pause(0.0001)
+
+        # check reaching goal
+        dist_to_goal = math.hypot(x[0] - goal[0], x[1] - goal[1])
+        if dist_to_goal <= config.robot_radius:
+            print("Goal!!")
+            break
+
+    print("Done")
+    if show_animation:
+        plt.plot(trajectory[:, 0], trajectory[:, 1], "-r")
+        plt.pause(0.0001)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main(robot_type=RobotType.circle)

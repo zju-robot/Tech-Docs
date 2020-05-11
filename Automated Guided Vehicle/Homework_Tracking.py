b892f38b358d37696f2cb5816605262c0ba56be5
NCJ <iamncj@outlook.com>
2020-05-11 23:56:22 +0800

    HW for AGV

---
+++ Automated Guided Vehicle/Homework_Tracking.py
@@ -0,0 +1,92 @@
+"""
+
+Tracking
+
+Author: lyl
+
+For learning purposes only
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from random import random
+
+# simulation parameters
+Kp_rho = 1.0
+Kp_alpha = 1.0
+Kp_beta = 1.0
+dt = 0.01
+
+show_animation = True
+
+
+def move_to_pose(x_start, y_start, theta_start, x_goal, y_goal, theta_goal):
+
+    # TODO
+
+    while rho > 0.001:
+        # TODO
+
+        if show_animation:  # pragma: no cover
+            plt.cla()
+            plt.arrow(x_start, y_start, np.cos(theta_start),
+                      np.sin(theta_start), color='r', width=0.1)
+            plt.arrow(x_goal, y_goal, np.cos(theta_goal),
+                      np.sin(theta_goal), color='g', width=0.1)
+            plot_vehicle(x, y, theta, x_traj, y_traj)
+
+
+def plot_vehicle(x, y, theta, x_traj, y_traj):  # pragma: no cover
+    # Corners of triangular vehicle when pointing to the right (0 radians)
+    p1_i = np.array([0.5, 0, 1]).T
+    p2_i = np.array([-0.5, 0.25, 1]).T
+    p3_i = np.array([-0.5, -0.25, 1]).T
+
+    T = transformation_matrix(x, y, theta)
+    p1 = np.matmul(T, p1_i)
+    p2 = np.matmul(T, p2_i)
+    p3 = np.matmul(T, p3_i)
+
+    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], 'k-')
+    plt.plot([p2[0], p3[0]], [p2[1], p3[1]], 'k-')
+    plt.plot([p3[0], p1[0]], [p3[1], p1[1]], 'k-')
+
+    plt.plot(x_traj, y_traj, 'b--')
+
+    # for stopping simulation with the esc key.
+    plt.gcf().canvas.mpl_connect('key_release_event',
+            lambda event: [exit(0) if event.key == 'escape' else None])
+
+    plt.xlim(0, 20)
+    plt.ylim(0, 20)
+
+    plt.pause(dt)
+
+
+def transformation_matrix(x, y, theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), x],
+        [np.sin(theta), np.cos(theta), y],
+        [0, 0, 1]
+    ])
+
+
+def main():
+
+    for i in range(5):
+        x_start = 20 * random()
+        y_start = 20 * random()
+        theta_start = 2 * np.pi * random() - np.pi
+        x_goal = 20 * random()
+        y_goal = 20 * random()
+        theta_goal = 2 * np.pi * random() - np.pi
+        print("Initial x: %.2f m\nInitial y: %.2f m\nInitial theta: %.2f rad\n" %
+              (x_start, y_start, theta_start))
+        print("Goal x: %.2f m\nGoal y: %.2f m\nGoal theta: %.2f rad\n" %
+              (x_goal, y_goal, theta_goal))
+        move_to_pose(x_start, y_start, theta_start, x_goal, y_goal, theta_goal)
+
+
+if __name__ == '__main__':
+    main()
